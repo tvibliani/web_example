16d0973f154531f0227cef1084bee3695516fdcd
Temur <tvibliani@users.noreply.github.com>
2014-10-12 18:34:48 +0400

    step-in: 3 (Adding a template xml)  Changes to be committed:
    	modified:   __openerp__.py
    	modified:   static/src/css/web_example.css
    	modified:   static/src/js/first_module.js
    	new file:   static/src/xml/web_example.xml

--- __openerp__.py
+++ __openerp__.py
@@ -7,4 +7,5 @@
     'data': ['web_example.xml'],
     'js': ['static/src/js/first_module.js'],
     'css': ['static/src/css/web_example.css'],
+    'qweb': ['static/src/xml/web_example.xml'],
 }
--- static/src/js/first_module.js
+++ static/src/js/first_module.js
@@ -2,10 +2,6 @@
 openerp.web_example = function (instance) {
     instance.web.client_actions.add('example.action', 'instance.web_example.Action');
     instance.web_example.Action = instance.web.Widget.extend({
-        className: 'oe_web_example',
-        start: function () {
-            this.$el.text("Hello, world!");
-            return this._super();
-        }
+        template: 'web_example.action'
     });
 };

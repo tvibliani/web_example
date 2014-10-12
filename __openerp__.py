1670a959aea4df960b2cb3c1494b2d94610159a1
Temur <tvibliani@users.noreply.github.com>
2014-10-12 18:03:32 +0400

    step-in: 3 (new menu 'Example Client Action')  Changes to be committed:
    	modified:   __openerp__.py
    	modified:   static/src/js/first_module.js
    	new file:   web_example.xml

--- __openerp__.py
+++ __openerp__.py
@@ -4,5 +4,6 @@
     'description': "Basic example of a (future) web module",
     'category': 'Hidden',
     'depends': ['web'],
+    'data': ['web_example.xml'],
     'js': ['static/src/js/first_module.js'],
 }

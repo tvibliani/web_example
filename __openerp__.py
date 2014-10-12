9ed0d3e9c69d798915b8ec189cfdda124e42aa71
Temur <tvibliani@users.noreply.github.com>
2014-10-12 20:07:22 +0400

    step: "Mic check, is this working?"     step-in: 1 (set up the basic testing structure)  Changes to be committed:
    	modified:   __openerp__.py
    	new file:   static/src/tests/timer.js

--- __openerp__.py
+++ __openerp__.py
@@ -8,4 +8,5 @@
     'js': ['static/src/js/first_module.js'],
     'css': ['static/src/css/web_example.css'],
     'qweb': ['static/src/xml/web_example.xml'],
+    'test': ['static/src/tests/timer.js'],
 }
---
+++ static/src/tests/timer.js
@@ -0,0 +1,8 @@
+openerp.testing.section('timer', function (test) {
+    test('successful test', function () {
+        ok(true, "should work");
+    });
+    test('unsuccessful test', function () {
+        ok(false, "shoud fail");
+    });
+});

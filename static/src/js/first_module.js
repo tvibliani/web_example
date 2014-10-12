bb3c2b7ac336d4da684d72babe3be1cd33d4381c
Temur <tvibliani@users.noreply.github.com>
2014-10-12 18:39:56 +0400

    step-in: 4 (hook some events on the buttons)  Changes to be committed:
    	modified:   static/src/js/first_module.js

--- static/src/js/first_module.js
+++ static/src/js/first_module.js
@@ -2,6 +2,18 @@
 openerp.web_example = function (instance) {
     instance.web.client_actions.add('example.action', 'instance.web_example.Action');
     instance.web_example.Action = instance.web.Widget.extend({
-        template: 'web_example.action'
+        template: 'web_example.action',
+        events: {
+            'click .oe_web_example_start button': 'watch_start',
+            'click .oe_web_example_stop button': 'watch_stop'
+        },
+        watch_start: function () {
+            this.$el.addClass('oe_web_example_started')
+                    .removeClass('oe_web_example_stopped');
+        },
+        watch_stop: function () {
+            this.$el.removeClass('oe_web_example_started')
+                    .addClass('oe_web_example_stopped');
+        },
     });
 };
